08fb8d0a45ffccc307815709080b6db3b17f5421
paul <pxl34232@gmail.com>
2020-07-31 18:53:35 -0400

    basic slides work

---
+++ index.js
@@ -0,0 +1,33 @@
+/* Making the slides work   */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); //leave the blank space befor active or boom!
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active"; //leave the blank space before active or shit goes wrong
+}
+/* Dealing with slides contents */
